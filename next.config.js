b0e2aace75df388aa2651ea7b1cac71a78200699
Anthony Salamanca <salamanca@cityinspain.dev>
2023-01-09 19:37:49 -0500

    add path rewrite for sanity

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  rewrites: async () => [
+    {
+      source: "/studio/:path*",
+      destination:
+        process.env.NODE_ENV === "development"
+          ? "http://localhost:3333/studio/:path*"
+          : "/studio/index.html",
+    },
+  ],
+  images: {
+    domains: ["cdn.sanity.io"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
